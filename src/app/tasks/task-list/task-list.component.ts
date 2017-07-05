02392a511c6164356e6f985a614d6a562d643ea0
cassiomolin <cassio.molin@gmail.com>
2017-07-05 20:25:38 +0100

    Animation improvements.

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -13,11 +13,11 @@ import {animate, style, transition, trigger} from "@angular/animations";
     trigger('fade', [
         transition(':enter', [
           style({'opacity': 0}),
-          animate('500ms', style({'opacity': 1}))
+          animate('400ms', style({'opacity': 1}))
         ]),
         transition(':leave', [
           style({'opacity': 1}),
-          animate('500ms', style({'opacity': 0}))
+          animate('400ms', style({'opacity': 0}))
         ])
     ])
   ]
