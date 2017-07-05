a72c1d7e3e0f8d5e66caaed0d3ece1ca060bf092
cassiomolin <cassio.molin@gmail.com>
2017-07-05 20:20:23 +0100

    Format animation metadata.

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -10,23 +10,16 @@ import {animate, style, transition, trigger} from "@angular/animations";
   styleUrls: ['./task-list.component.css'],
   providers: [TaskStatusPipe],
   animations: [
-    trigger(
-      'fade',
-      [
-        transition(
-          ':enter', [
-            style({'opacity': 0}),
-            animate('500ms', style({'opacity': 1}))
-          ]
-        ),
-        transition(
-          ':leave', [
-            style({'opacity': 1}),
-            animate('500ms', style({'opacity': 0}))
-          ]
-        )
-      ]
-    )
+    trigger('fade', [
+        transition(':enter', [
+          style({'opacity': 0}),
+          animate('500ms', style({'opacity': 1}))
+        ]),
+        transition(':leave', [
+          style({'opacity': 1}),
+          animate('500ms', style({'opacity': 0}))
+        ])
+    ])
   ]
 })
 export class TaskListComponent implements OnInit {
